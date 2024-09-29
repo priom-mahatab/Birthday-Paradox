adcb4ac18e4390a7540c6f098f1020919d8193c2
Zohaer Al Mahatab <133798007+priom-mahatab@users.noreply.github.com>
2024-09-29 00:43:03 -0500

    Add files via upload

---
+++ birthday_paradox.py
@@ -0,0 +1,33 @@
+import random
+def random_birthdays():
+    month = random.randint(1,12)
+    thirty_ones = [1, 3, 5, 7, 8, 10, 12]
+    if month == 2:
+        date = random.randint(1,28)
+    elif month in thirty_ones:
+        date = random.randint(1, 31)
+    else:
+        date = random.randint(1, 30)
+    birthday = f"{month}/{date}"
+    return birthday
+
+def birthday_matched(n):
+    birthdays = [random_birthdays() for _ in range(n)]
+    return len(birthdays) != len(set(birthdays))
+
+def birthday_paradox_simulation():
+    test_cases = list(range(5, 105, 5))
+    simulations = 1000
+
+    print("n\tProbability of at least one match")
+
+    for n in test_cases:
+        match_count = 0
+        for _ in range(simulations):
+            if birthday_matched(n):
+                match_count += 1
+
+        probability = match_count / simulations
+        print(f"{n}\t{probability:.2f}")
+
+birthday_paradox_simulation()
